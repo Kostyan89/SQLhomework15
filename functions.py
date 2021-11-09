49a8a1757009d4ebbeb600d4596acfd5a29049b4
Kostyan89 <kostasamin@gmail.com>
2021-11-09 15:21:04 +0200

    trying

--- functions.py
+++ functions.py
@@ -2,8 +2,8 @@ import sqlite3
 
 con = sqlite3.connect('animal.db')
 cursor = con.cursor()
-query_1 = "CREATE TABLE colors(id INTEGER PRIMARY KEY AUTOINCREMENT, name(VARCHAR(30))"
-query_2 = 'INSERT INTO colors(name) SELECT color_1 FROM animal'
+query_1 = 'CREATE TABLE colors(id INTEGER PRIMARY KEY AUTOINCREMENT, name(VARCHAR(30))'
+query_2 = 'INSERT INTO colors(name) SELECT color_1 FROM animals'
 cursor.execute(query_1)
 cursor.execute(query_2)
 con.close()
