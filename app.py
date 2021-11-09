65714e06029af63abb5d5cda8f38d586baec98e0
Kostyan89 <kostasamin@gmail.com>
2021-11-09 14:27:15 +0200

    add sqlite structure and end of flask app

--- app.py
+++ app.py
@@ -4,4 +4,17 @@ import sqlite3
 
 app = Flask(__name__)
 
-connection = sqlite3.connect('')
+
+@app.route('/item_id')
+def get_animal(id):
+
+
+connection = sqlite3.connect('animal.db')
+cursor = connection.cursor()
+sql = ('''наш sql запрос''')
+cursor.execute()
+connection.close()
+
+
+if __name__ == '__main__':
+    app.run()
