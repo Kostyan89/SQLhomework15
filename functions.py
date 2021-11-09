cc8c6e3efd4a3c94fd4943d18d6eecb1bcb955b1
Kostyan89 <kostasamin@gmail.com>
2021-11-09 16:33:48 +0200

    add some requests for SQL

--- functions.py
+++ functions.py
@@ -1,10 +1,17 @@
 import sqlite3
 
-con = sqlite3.connect('animal.db')
-cursor = con.cursor()
-query_1 = 'CREATE TABLE colors(id INTEGER PRIMARY KEY AUTOINCREMENT, name(VARCHAR(30))'
-query_2 = 'INSERT INTO colors(name) SELECT color_1 FROM animals'
-cursor.execute(query_1)
-cursor.execute(query_2)
-con.close()
+connection = sqlite3.connect('animal.db')
+cursor = connection.cursor()
+# query_1 = '''CREATE TABLE colors(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(30) )'''
+# query_2 = '''INSERT INTO colors (name) SELECT color1 FROM animals'''
+# query_3 = '''CREATE TABLE animal_colors(animal_id INT, color_id INT )'''
+# query_4 = '''INSERT INTO animal_colors SELECT animals."index", colors.id FROM animals JOIN colors ON color1 = colors.name'''
+query_5 = '''INSERT INTO animal_colors SELECT animals."index", colors.id FROM animals JOIN colors ON color2 = colors.name'''
+# cursor.execute(query_1)
+# cursor.execute(query_2)
+# cursor.execute(query_3)
+# cursor.execute(query_4)
+cursor.execute(query_5)
+# result = cursor.fetchall()
+connection.close()
 
