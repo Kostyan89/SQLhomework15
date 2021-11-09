8d296818c62fbb1a94ef9c684753a51a12f7f8c3
Kostyan89 <kostasamin@gmail.com>
2021-11-09 14:55:44 +0200

    .

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 from flask import Flask
 import sqlite3
 
-
 app = Flask(__name__)
-
-
-@app.route('/item_id')
-def get_animal(id):
-
-
 
 connection = sqlite3.connect('animal.db')
 cursor = connection.cursor()
-sql = ('''наш sql запрос''')
-cursor.execute()
+query_1 = "CREATE TABLE colors(id INTEGER PRIMARY KEY AUTOINCREMENT, name(VARCHAR(30))"
+query_2 = 'INSERT INTO colors(name) SELECT color_1 FROM animals'
+cursor.execute(query_1)
+cursor.execute(query_2)
 connection.close()
-
 
 if __name__ == '__main__':
     app.run()
